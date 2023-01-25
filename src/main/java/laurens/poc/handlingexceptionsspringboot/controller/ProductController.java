28a0780c358728ae4321182b1b4b5034ed101c75
Neyder M <neyder@gmail.com>
2023-01-25 13:51:18 -0500

    add lines

--- src/main/java/laurens/poc/handlingexceptionsspringboot/controller/ProductController.java
+++ src/main/java/laurens/poc/handlingexceptionsspringboot/controller/ProductController.java
@@ -24,6 +24,8 @@ public class ProductController {
 
     @GetMapping("/{id}")
     public Product getProductById(@PathVariable("id") long id) throws Exception {
+        String newVariable = "Hello";
+        System.out.println(newVariable);
         return this.productService.findProductById(id);
     }
 
